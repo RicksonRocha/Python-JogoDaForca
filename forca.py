ade0b47bd2b137dc28d8059020f2fdc51ef78411
Rickson <ricksonrocha0@gmail.com>
2021-11-18 17:58:10 -0300

    Implementando jogo da forca

--- forca.py
+++ forca.py
@@ -3,6 +3,21 @@ def jogar_forca():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = 'mochileiro'
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input('Chute uma letra')
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print('Letra {} encotrada na posicao {}'.format(letra, index))
+            index = index + 1
+
+        print('jogando')
+
     print("Fim do jogo")
 
 if (__name__ == '__main__'):
--- main.py
+++ main.py
@@ -1,54 +1,21 @@
-import random
+import forca
+import main #adivinhação
 
-def jogar_adivinhacao():
+def escolhe_jogo():
     print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
+    print("*******Escolha o seu jogo!*******")
     print("*********************************")
 
-    numero_secreto = round(random.randrange(1, 101))
-    rodada = 1
-    tentativas = 3
-    pontos = 1000
+    print("(1) Forca (2) Adivinhação")
 
-    print('Selecione o nível de dificuldade')
-    print('(1).Fácil (2).Médio (3).Difícil')
-    nivel = int(input('Digite o nível: '))
+    jogo = int(input("Qual jogo? "))
 
-    if (nivel == 1):
-        tentativas = 10
-    elif (nivel == 2):
-        tentativas = 7
-    else:
-        tentativas = 3
+    if (jogo == 1):
+        print("Jogando forca")
+        forca.jogar_forca()
+    elif (jogo == 2):
+        print("Jogando adivinhação")
+        main.jogar_adivinhacao()
 
-    while (rodada <= tentativas):
-        print('Tentativa', rodada, 'de', tentativas)
-
-        chute = input('Adivinhe o número entre 1 e 100')
-        chute_int = int(chute)
-
-        if (chute_int < 1 or chute_int > 100):
-            print('Apenas entre 1 e 100')
-            continue
-
-        acertou = chute_int == numero_secreto
-        maior = chute_int > numero_secreto
-        menor = chute_int < numero_secreto
-
-        if (acertou):
-            print('Voce acertou e fez {} pontos!'.format(pontos))
-            print('Acertou')
-            break
-        else:
-            if (maior):
-                print('Chute foi maior que o número secreto')
-            elif (menor):
-                print('Chute foi menor que o número secreto')
-            pontos_perdidos = abs(numero_secreto - chute_int)
-            pontos = pontos - pontos_perdidos
-        rodada = rodada + 1
-
-    print('Fim de jogo')
-
-if (__name__ == "__main__"):
-    jogar_adivinhacao()
+if (__name__ == '__main__'):
+    escolhe_jogo()
