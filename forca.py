39d7674ade790eb9cdf64b4fc338cf13aeedc1d8
Rickson <ricksonrocha0@gmail.com>
2021-11-19 17:20:00 -0300

    Refatorando: funcoes e implementacao do jogo da forca

--- forca.py
+++ forca.py
@@ -1,24 +1,58 @@
+import random
+
 def jogar_forca():
+    boas_vindas()
+    palavra_secreta = sorteia_palavra()
+
+    letras_acertadas = ["_" for letra in palavra_secreta] #list comprehennsions
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while(not enforcou and not acertou):
+        chute = input('Chute uma letra')
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+
+        enforcou = erros == 8
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+
+    if (acertou):
+        print('Voce acertou!')
+    else:
+        print('Voce perdeu!')
+
+    print("Fim do jogo")
+
+def boas_vindas():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = 'mochileiro'
-    enforcou = False
-    acertou = False
+def sorteia_palavra():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
 
-    while(not enforcou and not acertou):
-        chute = input('Chute uma letra')
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute == letra):
-                print('Letra {} encotrada na posicao {}'.format(letra, index))
-            index = index + 1
+    arquivo.close()
 
-        print('jogando')
-
-    print("Fim do jogo")
+    numero_sort = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero_sort].upper()
+    return palavra_secreta
 
 if (__name__ == '__main__'):
     jogar_forca()
