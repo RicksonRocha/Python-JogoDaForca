4544ca8290022b1b46436683655bc84e2c274ecf
Rickson <ricksonrocha0@gmail.com>
2021-11-22 10:10:57 -0300

    Correções

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import forca
-import main #adivinhação
 
-def escolhe_jogo():
+def iniciar_jogo():
     print("*********************************")
-    print("*******Escolha o seu jogo!*******")
+    print("*******JGOO DA FORCA!*******")
     print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+    jogar = int(input("Digite (1) para iniciar"))
 
-    jogo = int(input("Qual jogo? "))
-
-    if (jogo == 1):
+    if (jogar == 1):
         print("Jogando forca")
         forca.jogar_forca()
-    elif (jogo == 2):
-        print("Jogando adivinhação")
-        main.jogar_adivinhacao()
+    else:
+        print('Digite (1) para iniciar o jogo! Outras teclas não serão aceitas')
 
 if (__name__ == '__main__'):
-    escolhe_jogo()
+    iniciar_jogo()
